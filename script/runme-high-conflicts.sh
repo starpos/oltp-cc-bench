d4818ccc1c6ebfa952c1ec750f65e871459d49f7
HOSHINO Takashi <hoshino@labs.cybozu.co.jp>
2018-02-28 09:55:46 +0000

    update scripts.

--- script/runme-high-conflicts.sh
+++ script/runme-high-conflicts.sh
@@ -5,25 +5,28 @@ CXX=clang++-5.0
 #make clean
 #make CXX=$CXX DEBUG=0 MUTEX_ON_CACHELINE=1 LTO=1 -j
 
-make cmake_clean
-cmake -G Ninja . \
+if false; then
+  make cmake_clean
+  cmake -G Ninja . \
 -DCMAKE_CXX_COMPILER=clang++-5.0 \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DMUTEX_ON_CACHELINE=OFF \
+-DMUTEX_ON_CACHELINE=ON \
 -DLTO=ON
 ninja
+fi
 
 do_expr()
 {
-#for nrWr in 0 1 2 3 4 5 6 7 8 9 10; do
-#for nrWr in 1 5 10; do
-for nrWr in 0 1 5 10; do
-  for th in 96; do
-    for backoff in 0 1; do
+#for backoff in 0 1; do
+for backoff in 0; do
+  for rmw in 0 1; do
+    #for nrWr in 0 1 2 3 4 5 6 7 8 9 10; do
+    #for nrWr in 1 5 10; do
+    for nrWr in 0 1 5 10; do
       #for nrMu in 100 1000; do
       for nrMu in 100; do
-        for payload in 0 8 16 32 64 128 256 512 1024; do
-          for rmw in 0 1; do
+        for th in 96; do
+          for payload in 0 8 16 32 64 128 256 512 1024; do
             #for payload in 8; do
             # nrMu=50
             nrOp=10
@@ -33,16 +36,18 @@ for nrWr in 0 1 5 10; do
             #backoff=1
             #rmw=0
   
-            #./nowait_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -backoff $backoff -payload $payload -rmw $rmw
-            #./leis_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -vector 0 -lock 0 -payload $payload -rmw $rmw
-            #./leis_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -vector 1 -lock 0 -payload $payload -rmw $rmw
-            #./occ_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -backoff $backoff -payload $payload -rmw $rmw
-            #./tictoc_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -backoff $backoff -payload $payload -rmw $rmw
-            #./wait_die_bench -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -backoff $backoff -payload $payload -rmw $rmw
-            ./licc_bench -mode licc-pcc    -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -pqlock 0 -backoff $backoff -payload $payload -rmw $rmw
-            ./licc_bench -mode licc-occ    -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -pqlock 0 -backoff $backoff -payload $payload -rmw $rmw
-            ./licc_bench -mode licc-hybrid -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -pqlock 0 -backoff $backoff -payload $payload -rmw $rmw
-            ./licc_bench -mode licc-hybrid -th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -pqlock 7 -backoff $backoff -payload $payload -rmw $rmw
+            shared_opt="-th $th -mu $nrMu -p $period -loop $loop -nrop $nrOp -nrwr $nrWr -sm 5 -payload $payload -rmw $rmw"
+
+            #./nowait_bench $shared_opt -backoff $backoff
+            ./leis_bench $shared_opt -vector 0 -lock 0 
+            ./leis_bench $shared_opt -vector 1 -lock 0 
+            #./occ_bench $shared_opt -backoff $backoff
+            #./tictoc_bench $shared_opt -backoff $backoff
+            #./wait_die_bench $shared_opt -backoff $backoff
+            #./licc_bench $shared_opt -mode licc-pcc    -pqlock 0 -backoff $backoff
+            #./licc_bench $shared_opt -mode licc-occ    -pqlock 0 -backoff $backoff
+            #./licc_bench $shared_opt -mode licc-hybrid -pqlock 0 -backoff $backoff
+            #./licc_bench $shared_opt -mode licc-hybrid -pqlock 7 -backoff $backoff
           done
         done
       done
@@ -51,4 +56,4 @@ for nrWr in 0 1 5 10; do
 done
 }
  
-do_expr | tee -a high-conflicts.log.20180220c
+do_expr | tee -a high-conflicts.log.20180224a
--- script/runme-short-long-mix2-1m.sh
+++ script/runme-short-long-mix2-1m.sh
@@ -5,32 +5,49 @@ CXX=clang++-5.0
 #make clean
 #make CXX=$CXX DEBUG=0 LTO=1 MUTEX_ON_CACHELINE=0 -j
 
+if false; then
+  make cmake_clean
+  cmake -G Ninja . \
+-DCMAKE_CXX_COMPILER=clang++-5.0 \
+-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DMUTEX_ON_CACHELINE=OFF \
+-DLTO=ON
+ninja
+fi
+
+sleep 1
 
 do_expr() {
   th=96
   nrMu=1m
-  period=10
-  loop=10
-  longTxSize=1k
-  lm=9
+  period=100
+  loop=20
+  lm=5
+  longTxSize=100k
+  nrWr4Long=1k
+  sm=5
+  nrOp=4
+  nrWr=2
+  rmw=1
 
-#for longTxSize in 100 200 300 400 600 800 1k 2k 3k 4k 6k 8k 10k 20k 30k 40k 60k 80k 100k 200k 300k 400k 600k 800k 1m;
-for nrTh4LongTx in 1 2 3 4 5 6 7 8 9 10;
-#for longTxSize in 100 150 200 250 300 350 400 600 800 1k 2k 3k 4k 6k 8k 10k 20k 30k 40k 60k 80k 100k 200k 300k 400k 600k 800k 1m;
-do
-  for rmw in 0 1; do
-    ./occ_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm -rmw $rmw
-    ./tictoc_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm -rmw $rmw
-    ./licc_bench -mode licc-occ -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm -rmw $rmw
-    ./licc_bench -mode licc-pcc -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm -rmw $rmw
-    ./licc_bench -mode licc-hybrid -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -pqlock 0 -lm $lm -rmw $rmw
-    ./licc_bench -mode licc-hybrid -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -pqlock 7 -lm $lm -rmw $rmw
+  #for longTxSize in 100 200 300 400 600 800 1k 2k 3k 4k 6k 8k 10k 20k 30k 40k 60k 80k 100k 200k 300k 400k 600k 800k 1m;
+  #for longTxSize in 100 150 200 250 300 350 400 600 800 1k 2k 3k 4k 6k 8k 10k 20k 30k 40k 60k 80k 100k 200k 300k 400k 600k 800k 1m;
+  #for nrTh4LongTx in 1 2 3 4 5 6 7 8 9 10; do
+  #for nrTh4LongTx in 5 10 15 20 25 30 35 40 45 50; do
+  for nrTh4LongTx in 1 2 3 4 5 7 10 15; do
+    shared_opt="-th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -nrwr-long $nrWr4Long -th-long $nrTh4LongTx -lm $lm -sm $sm -nrop $nrOp -nrwr $nrWr -rmw $rmw"
+    #./occ_bench $shared_opt
+    #./tictoc_bench $shared_opt
+    #./licc_bench $shared_opt -mode licc-occ
+    #./licc_bench $shared_opt -mode licc-pcc 
+    #./licc_bench $shared_opt -mode licc-hybrid -pqlock 0
+    #./licc_bench $shared_opt -mode licc-hybrid -pqlock 7
+    #./nowait_bench $shared_opt 
+    #./leis_bench $shared_opt -vector 0
+    ./wait_die_bench $shared_opt
   done
-  ./nowait_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm
-  ./leis_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -vector 0 -lm $lm
-  ./nowait_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm
-  ./wait_die_bench -th $th -mu $nrMu -p $period -loop $loop -long-tx-size $longTxSize -th-long $nrTh4LongTx -lm $lm
-done
 }
 
-do_expr | tee -a short-long-mix2-1m.log.20180117c
+do_expr | tee -a short-long-mix2-1m.log.20180227e
+
+#./licc_bench -th 96 -mu 1m -p 10 -loop 1 -long-tx-size 100k -th-long 10 -lm 0 -rmw 0 -nrop 100 -nrwr 10 -sm 5  -mode licc-hybrid
