fcd46d2b13eec3007666a370a6641c2376bd3e3e
HOSHINO Takashi <starpos@gmail.com>
2020-03-23 14:48:58 +0900

    add sleep_us().

--- include/sleep.hpp
+++ include/sleep.hpp
@@ -6,3 +6,9 @@ void sleep_ms(size_t ms)
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(ms));
 }
+
+
+void sleep_us(size_t us)
+{
+    std::this_thread::sleep_for(std::chrono::microseconds(us));
+}
