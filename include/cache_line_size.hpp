6803c9062095477b714c396dca4dc7b17f678108
HOSHINO Takashi <starpos@gmail.com>
2020-04-03 12:07:43 +0900

    add CacheLineAligned template.

--- include/cache_line_size.hpp
+++ include/cache_line_size.hpp
@@ -1,4 +1,32 @@
 #pragma once
 #include <cstddef>
+#include <type_traits>
 
-const size_t CACHE_LINE_SIZE = 64;
+
+constexpr size_t CACHE_LINE_SIZE = 64;
+
+
+template <typename T, typename Enabled = void>
+struct CacheLineAligned
+{
+    alignas(CACHE_LINE_SIZE)
+    T value;
+};
+
+
+template <typename T>
+struct CacheLineAligned<T, std::enable_if_t<std::is_scalar_v<T>>>
+{
+    alignas(CACHE_LINE_SIZE)
+    T value;
+
+    CacheLineAligned() = default;
+
+    // For implicit conversion from/to data of type T.
+    CacheLineAligned(T value0) noexcept : value(value0) {}
+    operator T() const noexcept { return value; }
+
+    T operator++() noexcept { return ++value; }
+    T operator++(int) noexcept { return value++; }
+    // TODO: overrides of primitive operators if need.
+};
