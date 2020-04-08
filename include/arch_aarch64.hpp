64115478b4a0ad4f339acfcd0c25075b2c8318d0
HOSHINO Takashi <starpos@gmail.com>
2020-04-08 16:14:09 +0900

    arch_aarch64: add aarch64_stlxr().

--- include/arch_aarch64.hpp
+++ include/arch_aarch64.hpp
@@ -30,25 +30,61 @@ INLINE void aarch64_wfe()
 }
 
 
+/**
+ * You can use this as load-link.
+ */
 template <typename UInt>
-INLINE UInt aarch64_ldaxr(const UInt& val)
+INLINE UInt aarch64_ldaxr(const UInt& target)
 {
-    uint64_t old;
+    uint64_t val;
     switch (sizeof(UInt)) {
     case 8:
-        __asm__ volatile ("ldaxr %0, [%1]" : "=&r" (old) : "r" (&val) : "memory");
+        __asm__ volatile ("ldaxr %0, [%1]" : "=&r" (val) : "r" (&target) : "memory");
         break;
     case 4:
-        __asm__ volatile ("ldaxr %w0, [%1]" : "=&r" (old) : "r" (&val) : "memory");
+        __asm__ volatile ("ldaxr %w0, [%1]" : "=&r" (val) : "r" (&target) : "memory");
         break;
     case 2:
-        __asm__ volatile ("ldaxrh %w0, [%1]" : "=&r" (old) : "r" (&val) : "memory");
+        __asm__ volatile ("ldaxrh %w0, [%1]" : "=&r" (val) : "r" (&target) : "memory");
         break;
     case 1:
-        __asm__ volatile ("ldaxrb %w0, [%1]" : "=&r" (old) : "r" (&val) : "memory");
+        __asm__ volatile ("ldaxrb %w0, [%1]" : "=&r" (val) : "r" (&target) : "memory");
         break;
     default:
         assert(true);
     }
-    return UInt(old);
+    return UInt(val);
+}
+
+
+/**
+ * You can use this as store-conditional.
+ * return: true if success.
+ */
+template <typename UInt1, typename UInt2>
+INLINE bool aarch64_stlxr(UInt1& target, UInt2 val)
+{
+    uint32_t status;
+    uint64_t val2 = val;
+    switch (sizeof(UInt1)) {
+    case 8:
+        __asm__ volatile ("stlxr %w0, %1, [%2]"
+                          : "=&r" (status) : "r" (val2), "r" (&target) : "memory");
+        break;
+    case 4:
+        __asm__ volatile ("stlxr %w0, %w1, [%2]"
+                          : "=&r" (status) : "r" (val2), "r" (&target) : "memory");
+        break;
+    case 2:
+        __asm__ volatile ("stlxrh %w0, %w1, [%2]"
+                          : "=&r" (status) : "r" (val2), "r" (&target) : "memory");
+        break;
+    case 1:
+        __asm__ volatile ("stlxrb %w0, %w1, [%2]"
+                          : "=&r" (status) : "r" (val2), "r" (&target) : "memory");
+        break;
+    default:
+        assert(true);
+    }
+    return status == 0;
 }
