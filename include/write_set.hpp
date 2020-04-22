1664e60c87f3018a47203de1bd84e1649ff425f9
HOSHINO Takashi <starpos@gmail.com>
2020-04-22 17:35:48 +0900

    write_set: use INLINE.

--- include/write_set.hpp
+++ include/write_set.hpp
@@ -16,31 +16,31 @@ struct LocalValInfo
     size_t localValIdx; // UINT64_MAX means empty.
     void *sharedVal; // If localValIdx != UINT64_MAX, it must not be null.
 
-    LocalValInfo() : localValIdx(UINT64_MAX), sharedVal(nullptr) {
+    INLINE LocalValInfo() : localValIdx(UINT64_MAX), sharedVal(nullptr) {
     }
-    LocalValInfo(size_t localValIdx0, void *sharedVal0)
+    INLINE LocalValInfo(size_t localValIdx0, void *sharedVal0)
         : localValIdx(localValIdx0), sharedVal(sharedVal0) {
     }
-    LocalValInfo(const LocalValInfo&) = default;
-    LocalValInfo& operator=(const LocalValInfo&) = default;
-    LocalValInfo(LocalValInfo&& rhs) noexcept : LocalValInfo() {
+    INLINE LocalValInfo(const LocalValInfo&) = default;
+    INLINE LocalValInfo& operator=(const LocalValInfo&) = default;
+    INLINE LocalValInfo(LocalValInfo&& rhs) noexcept : LocalValInfo() {
         swap(rhs);
     }
-    LocalValInfo& operator=(LocalValInfo&& rhs) noexcept {
+    INLINE LocalValInfo& operator=(LocalValInfo&& rhs) noexcept {
         swap(rhs);
         return *this;
     }
 
-    void set(size_t localValIdx0, void *sharedVal0) {
+    INLINE void set(size_t localValIdx0, void *sharedVal0) {
         localValIdx = localValIdx0;
         sharedVal = sharedVal0;
     }
-    void reset() {
+    INLINE void reset() {
         localValIdx = UINT64_MAX;
         sharedVal = nullptr;
     }
 
-    void swap(LocalValInfo& rhs) noexcept {
+    INLINE void swap(LocalValInfo& rhs) noexcept {
         std::swap(localValIdx, rhs.localValIdx);
         std::swap(sharedVal, rhs.sharedVal);
     }
@@ -57,20 +57,20 @@ struct OpEntry
     Lock lock;
     LocalValInfo info;
 
-    OpEntry() : lock(), info() {
+    INLINE OpEntry() : lock(), info() {
     }
-    explicit OpEntry(Lock&& lock0) : lock(std::move(lock0)), info() {
+    INLINE explicit OpEntry(Lock&& lock0) : lock(std::move(lock0)), info() {
     }
     OpEntry(const OpEntry&) = delete;
     OpEntry& operator=(const OpEntry&) = delete;
-    OpEntry(OpEntry&& rhs) noexcept : OpEntry() {
+    INLINE OpEntry(OpEntry&& rhs) noexcept : OpEntry() {
         swap(rhs);
     }
-    OpEntry& operator=(OpEntry&& rhs) noexcept {
+    INLINE OpEntry& operator=(OpEntry&& rhs) noexcept {
         swap(rhs);
         return *this;
     }
-    void swap(OpEntry& rhs) noexcept {
+    INLINE void swap(OpEntry& rhs) noexcept {
         std::swap(lock, rhs.lock);
         std::swap(info, rhs.info);
     }
