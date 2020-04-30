a24da9606de39e44a6b5d10e88c01ed0316880c2
HOSHINO Takashi <starpos@gmail.com>
2020-05-01 01:09:47 +0900

    add get_rdtscp_freq command.

---
+++ get_rdtscp_freq.cpp
@@ -0,0 +1,12 @@
+#include "time.hpp"
+#include "sleep.hpp"
+
+
+int main()
+{
+    uint64_t t0 = cybozu::time::rdtscp();
+    sleep_ms(1000);
+    uint64_t t1 = cybozu::time::rdtscp();
+
+    ::printf("%" PRIu64 " count/sec\n", t1 - t0);
+}
